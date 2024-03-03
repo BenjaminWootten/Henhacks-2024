f706dcf2dc1250bf680596b691940915542409a1
BenjaminWootten <bwootten@udel.edu>
2024-03-02 20:49:19 -0500

    Revert "Revert "fixed broken water purifier""
    This reverts commit 16d811d8c70628f58e71d54fa583aaef9c261075.

--- minigames/moss.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-func _on_mo_area_entered(area):
-	self.visible=false
-	pass # Replace with function body.
--- minigames/sand.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-func _on_sa_area_entered(area):
-	self.visible=false
-	pass # Replace with function body.
