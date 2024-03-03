56ad47fed2e11ef9e8ed86360428e22c5d9362b2
magnusculley <magnus@udel.edu>
2024-03-03 06:18:04 -0500

    i dont remmeber

--- assets/UI/LevelSelect.gd
+++ assets/UI/LevelSelect.gd
@@ -2,11 +2,11 @@ extends Control
 @onready var global = get_node("/root/Global")
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _ready():
 	if(global.tent_completed==true):
 		$Sprite2D5.visible=true
+	if(global.water_completed==true):
+		$Sprite2D6.visible=true
--- minigames/waterPurifier.gd
+++ minigames/waterPurifier.gd
@@ -8,6 +8,11 @@ extends Node2D
 @onready var bottle_sprite=$bottle/AnimatedSprite2D
 @onready var box=$Control
 @onready var label=$Control/MarginContainer/MarginContainer/Label
+@onready var tip=$PanelContainer/HBoxContainer/Label
+@onready var global = get_node("/root/Global")
+
+func _ready():
+	tip.text="Water purifiers work by filtering out the largest particles first."
 
 func _on_moss_area_entered(area):
 	if bottle_sprite.frame == 0:
@@ -30,6 +35,8 @@ func _on_coarse_area_entered(area):
 		bottle_sprite.frame = 4
 
 func _on_stone_area_entered(area):
+	if bottle_sprite.frame == 0:
+		tip.text="Start with something smaller."
 	if bottle_sprite.frame == 4:
 		stone.visible = false
 		bottle_sprite.frame = 5
@@ -37,6 +44,7 @@ func _on_stone_area_entered(area):
 
 func bottle_completed():
 	$Button.visible=true
+	global.water_completed=true
 
 func _on_button_pressed():
 	bottle_sprite.frame =6
@@ -44,30 +52,40 @@ func _on_button_pressed():
 func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://assets/UI/LevelSelect.tscn")
 func _on_stone_mouse_entered():
+	tip.text="Stones are good for filtering out the largest particles."
 	box.visible=true
 	label.text="stones"
 func _on_stone_mouse_exited():
 	box.visible=false
+	tip.text="Water purifiers work by filtering out the largest particles first."
 func _on_moss_mouse_entered():
+	tip.text="Cloth works well to remove the last of the solid particles."
 	box.visible=true
 	label.text="cloth"
 func _on_moss_mouse_exited():
 	box.visible=false
+	tip.text="Water purifiers work by filtering out the largest particles first."
 func _on_sand_mouse_entered():
+	tip.text="Fine sand filters out and absorbs very small particles."
 	box.visible=true
 	label.text="fine sand"
 func _on_sand_mouse_exited():
 	box.visible=false
+	tip.text="Water purifiers work by filtering out the largest particles first."
 func _on_coal_mouse_entered():
+	tip.text="Activated charcoal is able to remove organic chemicals from dirty water."
 	box.visible=true
 	label.text="charcoal"
 func _on_coal_mouse_exited():
 	box.visible=false
+	tip.text="Water purifiers work by filtering out the largest particles first."
 func _on_coarse_mouse_entered():
 	box.visible=true
+	tip.text="Coarse sand can filter out large suspended particles in the water."
 	label.text="coarse sand"
 func _on_coarse_mouse_exited():
 	box.visible=false
+	tip.text="Water purifiers work by filtering out the largest particles first."
 
 
 
