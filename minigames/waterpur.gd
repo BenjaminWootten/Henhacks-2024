2d377738776795e31f9fb464b714013aed777ea6
BenjaminWootten <bwootten@udel.edu>
2024-03-02 20:25:08 -0500

    improved dragging on water purifier

--- minigames/waterpur.gd
+++ minigames/waterpur.gd
@@ -6,60 +6,21 @@ extends Node2D
 @onready var moss=$moss
 @onready var bottles=$bottle/AnimatedSprite2D
 
-var grabottle=false
 var grabmoss=false
 var grabstone=false
 var grabsand=false
-
-func _process(delta):
-	if grabottle==true:
-		bottle.position=get_local_mouse_position()
-	if grabmoss==true:
-		moss.position=get_local_mouse_position()
-	if grabsand==true:
-		sand.position=get_local_mouse_position()
-	if grabstone==true:
-		stone.position=get_local_mouse_position()
-
-func _on_bottle_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("click"):
-		if grabottle==true:
-			grabottle=false
-		else:
-			grabottle=true
-
-func _on_moss_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("click"):
-		if grabmoss==true:
-			grabmoss=false
-		else:
-			grabmoss=true
-
-func _on_stone_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("click"):
-		if grabstone==true:
-			grabstone=false
-		else:
-			grabstone=true
-
-func _on_sand_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("click"):
-		if grabsand==true:
-			grabsand=false
-		else:
-			grabsand=true
 
 func _on_moss_area_entered(area):
 	if bottles.frame==0:
 		moss.visible=false
 		bottles.frame=1
 
-func _on_sand_area_entered(area):
+func _on_sand_area_entered(_area):
 	if bottles.frame==1:
 		sand.visible=false
 		bottles.frame=2.
 
-func _on_stone_area_entered(area):
+func _on_stone_area_entered(_area):
 	if bottles.frame==2:
 		stone.visible=false
 		bottles.frame=3
--- utility/draggable_object.gd
+++ utility/draggable_object.gd
@@ -2,13 +2,13 @@ extends Area2D
 
 var dragging = false
 
-func _on_input_event(viewport, event, shape_idx):
+func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			dragging = true
 		else:
 			dragging = false
 
-func _process(delta):
+func _process(_delta):
 	if dragging:
 		global_position = get_global_mouse_position()
